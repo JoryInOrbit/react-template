bb64a59d1003f60944dbf310c6d184e6ba18dae7
JoryInOrbit <maldoyka@abv.bg>
2025-04-07 15:28:05 +0300

    updated core structure

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -3,9 +3,15 @@ import '../styles/App.css'
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className='h-[100vh] flex flex-col'>
+      <header className='h-20 flex flex-row justify-center items-center text-3xl'>React App</header>
+
+      <main className='grow-1'>
+        
+      </main>
+    
+      <footer className='h-15 flex flex-row justify-center items-center'>React app</footer>
+    </div >
   )
 }
 
